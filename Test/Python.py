86d0d214d391035bd13b98c5061e649737a852ca
Photonico <LukeNiu@outlook.com>
2022-03-30 16:28:37 +1100

    Update

--- Test/Python.py
+++ Test/Python.py
@@ -91,12 +91,12 @@ import time
 import matplotlib.pyplot as plt
 
 """
-Sn = 1 + 1/2^2 + 1/3^2 + 1/4^2 + ... + 1/n^2
-lim(n->∞)Sn = π^2/6 ≈ 1.6449340668482264
+    Sn = 1 + 1/2^2 + 1/3^2 + 1/4^2 + ... + 1/n^2
+    lim(n->∞)Sn = π^2/6 ≈ 1.6449340668482264
 
-R1(n) = ((n+1)*S(n+1)-n*S(n))/np.math.factorial(1)
-R2(n) = ((n+2)^2*S(n+1)-2*(n+1)^2*S(n+1)+n^2*S(n))/np.math.factorial(2)
-……
+    R1(n) = ((n+1)*S(n+1)-n*S(n))/np.math.factorial(1)
+    R2(n) = ((n+2)^2*S(n+1)-2*(n+1)^2*S(n+1)+n^2*S(n))/np.math.factorial(2)
+    ……
 """
 
 to = time.time()
