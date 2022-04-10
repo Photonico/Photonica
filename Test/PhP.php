610ad245426c6677440305784be5ff3546949ab3
Photonico <LukeNiu@outlook.com>
2022-04-10 23:22:37 +1000

    Update

---
+++ Test/PhP.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1); // strict requirement ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function sum(int $x, int $y) {
+  $z = $x + $y;
+  return $z;
+}
+
+echo "5 + 10 = " . sum(5,10) . "<br>";
+echo "7 + 13 = " . sum(7,13) . "<br>";
+echo "2 + 4 = " . sum(2,4);
+?>
+
+</body>
+</html>
